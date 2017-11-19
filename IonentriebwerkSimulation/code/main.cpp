d92b928297aad4446b62968a51f94866d8400e8e
Michael Hollmer <michaelhollmer@freenet.de>
2017-11-19 13:34:27 +0100

    Added GUI output. The program displays now the current speed, ampere, volt, and force.

--- IonentriebwerkSimulation/code/main.cpp
+++ IonentriebwerkSimulation/code/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <sstream>
 
 /* Engines */
 #include <irrlicht.h>
 #include <SFML\Graphics.hpp>
 
 /* Prototypes */
-void updateIons(irr::scene::ISceneManager *manager, irr::scene::IAnimatedMesh *ionMesh, std::vector<irr::scene::IAnimatedMeshSceneNode*>* ionStorage, std::vector<float>* ionsVelocity, sf::Clock* timer, int creationSpeed, float acceleration);
+void updateIons(irr::scene::ISceneManager *manager, irr::scene::IAnimatedMesh *ionMesh, std::vector<irr::scene::IAnimatedMeshSceneNode*>* ionStorage, std::vector<float>* ionsVelocity, sf::Clock* timer, irr::gui::IGUIStaticText *forceText, 
+				irr::gui::IGUIStaticText *ampereText, irr::gui::IGUIStaticText *voltText, irr::gui::IGUIStaticText *velocityText, int creationSpeed, float acceleration);
 
 int main()
 {
@@ -60,9 +62,14 @@ int main()
 	
 	irr::gui::IGUIScrollBar *scrollBarAmount = gui->addScrollBar(true, irr::core::rect<irr::s32>(10, 70, 300, 90), 0, -1);
 	scrollBarAmount->setMax(20);
-	scrollBarAmount->setMin(1);
+	scrollBarAmount->setMin(0);
 	gui->addStaticText(irr::core::stringw("Anzahl Ionen pro Sekunde in *10^13 Min: 0 Max: 1 * 10^15 ").c_str(), irr::core::rect<irr::s32>(320, 70, 670, 90), true, true, 0, -1, false);
-
+	
+	/* Output */
+	irr::gui::IGUIStaticText *force = gui->addStaticText(irr::core::stringw("0").c_str(), irr::core::rect<irr::s32>(10, 100, 100, 120), true, true, 0, -1, false);
+	irr::gui::IGUIStaticText *ampere = gui->addStaticText(irr::core::stringw("0").c_str(), irr::core::rect<irr::s32>(10, 130, 100, 150), true, true, 0, -1, false);
+	irr::gui::IGUIStaticText *volt = gui->addStaticText(irr::core::stringw("0").c_str(), irr::core::rect<irr::s32>(10, 160, 100, 180), true, true, 0, -1, false);
+	irr::gui::IGUIStaticText *velocity = gui->addStaticText(irr::core::stringw("0").c_str(), irr::core::rect<irr::s32>(10, 190, 100, 210), true, true, 0, -1, false);
 
 	/* ########################################### Main Loop ########################################### */
 	while (device->run())
@@ -76,7 +83,7 @@ int main()
 
 		/* Update */
 		light1->setPosition(camera->getPosition());
-		updateIons(manager, ionMesh, &ions, &ionsVelocity, &timer, ionsPerSecond, acceleration);
+		updateIons(manager, ionMesh, &ions, &ionsVelocity, &timer, force, ampere, volt, velocity, ionsPerSecond, acceleration);
 
 		acceleration = scrollBarVolt->getPos() / 100.0f;
 		ionsPerSecond = scrollBarAmount->getPos();
@@ -85,14 +92,18 @@ int main()
 	}
 }
 
-void updateIons(irr::scene::ISceneManager *manager, irr::scene::IAnimatedMesh *ionMesh, std::vector<irr::scene::IAnimatedMeshSceneNode*>* ionStorage, std::vector<float>* ionsVelocity, sf::Clock* timer, int creationSpeed, float acceleration)
+void updateIons(irr::scene::ISceneManager *manager, irr::scene::IAnimatedMesh *ionMesh, std::vector<irr::scene::IAnimatedMeshSceneNode*>* ionStorage, std::vector<float>* ionsVelocity, sf::Clock* timer, irr::gui::IGUIStaticText *forceText, 
+				irr::gui::IGUIStaticText *ampereText, irr::gui::IGUIStaticText *voltText, irr::gui::IGUIStaticText *velocityText, int creationSpeed, float acceleration)
 {
 	/* Create new ions */
-	if (timer->getElapsedTime().asMilliseconds() > sf::milliseconds(1000 / creationSpeed).asMilliseconds()) //Check every second 
+	if (creationSpeed != 0) //Never ever devide by zero
 	{
-		ionStorage->push_back(manager->addAnimatedMeshSceneNode(ionMesh, 0, -1, irr::core::vector3df(0 + ((rand() % 2) - 0.5f) + ((rand() % 2) - 0.5f), 5 + ((rand() % 2) - 0.5f) + ((rand() % 2) - 0.5f), -5.5f + ((rand() % 2) - 0.5f) + ((rand() % 2) - 0.5f)), irr::core::vector3df(0, 0, 0), irr::core::vector3df(0.2f, 0.2f, 0.2f), false));
-		ionsVelocity->push_back(0.05f);
-		timer->restart();
+		if (timer->getElapsedTime().asMilliseconds() > sf::milliseconds(1000 / creationSpeed).asMilliseconds()) //Check every second 
+		{
+			ionStorage->push_back(manager->addAnimatedMeshSceneNode(ionMesh, 0, -1, irr::core::vector3df(0 + ((rand() % 2) - 0.5f) + ((rand() % 2) - 0.5f), 5 + ((rand() % 2) - 0.5f) + ((rand() % 2) - 0.5f), -5.5f + ((rand() % 2) - 0.5f) + ((rand() % 2) - 0.5f)), irr::core::vector3df(0, 0, 0), irr::core::vector3df(0.2f, 0.2f, 0.2f), false));
+			ionsVelocity->push_back(0.05f);
+			timer->restart();
+		}
 	}
 
 	/* Update all the existing ions */
@@ -109,5 +120,50 @@ void updateIons(irr::scene::ISceneManager *manager, irr::scene::IAnimatedMesh *i
 			ionsVelocity->erase(ionsVelocity->begin() + i, ionsVelocity->begin() + i + 1);
 		}
 	}
+
+	/* Calculate force */
+	float ionsAmount = static_cast<float>(creationSpeed * pow(10, 13));
+
+	float ampere = static_cast<float>(ionsAmount * 1.602 * pow(10, -19));
+	//std::cout << "Ampere: " << ampere << std::endl;
+
+	float volt = ((acceleration * 100) - 99) * 19.60f;
+	//std::cout << "Volt: " << volt << std::endl;
+
+	float velocity = static_cast<float>(sqrt((2 * 1.602 * pow(10, -19) * volt) / (14 * 1.66054 * pow(10, -27))));
+	//std::cout << "Geschwindigkeit: " << velocity << std::endl;
+
+	float force = static_cast<float>((14 * (1.66054 * pow(10, -27))) * (ampere / (1.602 * pow(10, -19))) * velocity);
+	//std::cout << "Kraft: " << force << std::endl;
+
+	std::string forceStr;
+	std::stringstream forceStream;
+	forceStream << force;
+	forceStream >> forceStr;
+	forceStr = forceStr + " N";
+
+	std::string ampereStr;
+	std::stringstream ampereStream;
+	ampereStream << ampere;
+	ampereStream >> ampereStr;
+	ampereStr = ampereStr + " A";
+
+	std::string voltStr;
+	std::stringstream voltStream;
+	voltStream << volt;
+	voltStream >> voltStr;
+	voltStr = voltStr + " V";
+
+	std::string velocityStr;
+	std::stringstream velocityStream;
+	velocityStream << velocity;
+	velocityStream >> velocityStr;
+	velocityStr = velocityStr + " m/s";
+
+	forceText->setText(irr::core::stringw(forceStr.c_str()).c_str());
+	ampereText->setText(irr::core::stringw(ampereStr.c_str()).c_str());
+	voltText->setText(irr::core::stringw(voltStr.c_str()).c_str());
+	velocityText->setText(irr::core::stringw(velocityStr.c_str()).c_str());
+
 }
 
